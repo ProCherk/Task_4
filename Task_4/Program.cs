0e1216867cd45ca030d1252310ea73201110056b
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-10-19 17:48:14 +0300

    Можливість вводу данних з файлу

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Transactions;
+using System.IO;
 
 namespace Task_4
 {
@@ -6,22 +8,58 @@ namespace Task_4
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.WriteLine("Оберіть варіант вводу: \n1 - з клавіатури\n2 - з файлу\nВвід: ");
+            string enter = Console.ReadLine();
 
-            Money cash1 = new Money(23.7);
-            Money cash2 = new Money(40, 5);
-            /*
-            cash1.GetInfo();
-            cash2.GetInfo();
+            do
+            {
+                if (enter == "1")
+                {
+                    //Введення данних вручну
+                    Console.WriteLine("Введіть грошову суму:");
+                    double a = Convert.ToDouble(Console.ReadLine());
 
-            cash1.SumMoney(23.1, 45.5);
-            cash2.SumMoney(20, 90, 31, 45);
-            */
-            cash1.ToCoin(34.23);
-            cash2.ToPaperMoney(23423);
+                    Money cash1 = new Money(a);
+
+                    cash1.GetInfo();
+
+                    cash1.SumMoney(a, 45.5);
+
+                    cash1.ToCoin(a);
+                    cash1.ToPaperMoney(23345);
+                    break;
+
+                }
+                else if (enter == "2")
+                {
+                    //Отримання інформації з файлу
+                    FileStream f = new FileStream("MoneyInfo.txt", FileMode.Open, FileAccess.Read);
+                    StreamReader fr = new StreamReader(f);
+                    string info = fr.ReadLine();
+                    double a = Convert.ToDouble(info);
+                    fr.Close();
+
+
+                    Money cash1 = new Money(a);
+
+                    cash1.GetInfo();
+
+                    cash1.SumMoney(a, 45.5);
+
+                    cash1.ToCoin(a);
+                    cash1.ToPaperMoney(23345);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Оберіть варіант вводу: \n1 - з клавіатури\n2 - з файлу\nВвід: ");
+                    enter = Console.ReadLine();
+                }
+            } while (true);
+
             
-            //Console.WriteLine(23.12 % 100 / 100);
-
         }
     }
 
@@ -30,28 +68,13 @@ namespace Task_4
         private double p_money; 
         private double coin;
         private double summa;
-        private int summa_coin;
-        private int summa_pm;
-
-        //Введення грошової суми можливе у двох форматах 1 - як дріб, 2 - окремо гривні і копійки
+        
+        //Введення грошової суми 
         public Money(double a) {
             if (a > 0)
             {   
                 p_money = Math.Truncate(a);
-                coin = 100 * (a - p_money);
-            }
-            else
-            {
-                p_money = 0;
-                coin = 0;
-            }
-        }
-
-        public Money(int a, int b) {
-            if (a > 0 & b >= 0)
-            {
-                p_money = a;
-                coin = b;
+                coin = a * 100 - p_money;
             }
             else
             {
@@ -67,32 +90,17 @@ namespace Task_4
         }
 
         //Обчислення суми
-        //1 варіант, введення 2 дробових значень
         public void SumMoney(double a, double b)
         {
             summa = a + b;
             Console.WriteLine($"Сума: {a} грн. + {b} грн. = {summa} грн.");
         }
 
-        //2 варіант, введення окремо гривень і копійок
-        public void SumMoney(int a, int b, int c, int d)
-        {
-            summa_coin = b + d;
-            if (summa_coin > 100)
-            {
-                summa_coin -= 100;
-                summa_pm = 1;
-            }
-
-            summa_pm += a + c;
-            Console.WriteLine($"Сума: {a} грн. {b} коп. + {c} грн. {d} коп. = {summa_pm} грн. {summa_coin} коп.");
-        }
-         //Переведення у копійки
+        //Переведення у копійки
         public void ToCoin(double a)
         {
-            coin = Math.Truncate(a);
-            coin += (a - coin)*100;
-            Math.Ceiling(coin);
+            coin = a * 100;
+            coin = Math.Truncate(coin) + 1;
             Console.WriteLine($"{a} гривень у копійках = {coin} копійок");
         }
 
